8f86f46997dba56bb87d3cc6e15766f83dec7969
uba-gabriel <gabrielcarutti@gmail.com>
2023-08-16 23:50:53 -0300

    creo directiva y edito con lo de brian ducca el detalle sensor

--- app-fullstack-base-2022-i07/src/frontend/dam/src/app/app.module.ts
+++ app-fullstack-base-2022-i07/src/frontend/dam/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { SombreadoDirective } from './directivas/sombreado.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SombreadoDirective],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
---
+++ app-fullstack-base-2022-i07/src/frontend/dam/src/app/directivas/sombreado.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SombreadoDirective } from './sombreado.directive';
+
+describe('SombreadoDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SombreadoDirective();
+    expect(directive).toBeTruthy();
+  });
+});
